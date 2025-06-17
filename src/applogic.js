aa6f66e4f97c38a0d2d76ccb6b5ad9f128c13cc8
Ulysse <ulysse.aubanton@gmail.com>
2025-06-17 19:19:42 +0200

    Add completed property and toggle method

--- src/applogic.js
+++ src/applogic.js
@@ -4,5 +4,10 @@ export class Todo {
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
-    }
+        this.completed = false;
+    };
+
+    toggleCompleted(){
+        this.completed = this.completed? false: true;
+    };
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import { Todo } from "./applogic";
 
-const firstTodo = new Todo("work", "i need to work duh", new Date(2000,5,26), 1);
+const firstTodo = new Todo("work", "i just need to work duh", new Date(2000,5,26), 1);
+firstTodo.toggleCompleted();
 console.log(firstTodo);
