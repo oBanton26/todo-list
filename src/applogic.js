0ad6cbcc32c7120a8ff96b74b0d8ce1c967d3213
Ulysse <ulysse.aubanton@gmail.com>
2025-06-18 13:41:24 +0200

    Add createTodo function and defaultList
    The goal is to always create a Todo inside of a List. So even if not
    defined, it will be stored inside of the defaultList.

--- src/applogic.js
+++ src/applogic.js
@@ -26,4 +26,14 @@ export class List {
         const todoIndex = this.array.indexOf(todo);
         this.array.splice(todoIndex, 1);
     };
-}
+};
+
+// The purpose of the defaultList is to contain all the Todos that don't fit 
+// inside a specific list. It is NOT the generalList which is for displaying 
+// all of the Todos (from defaultList and other specified lists)
+export const defaultList = new List("Default List");
+
+export function createTodo(title, description = "", dueDate = new Date(), priority = 3, list = defaultList){
+    const newTodo = new Todo(title, description, dueDate, priority);
+    list.addTodo(newTodo);
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import "./styles.css";
-import { Todo, List} from "./applogic";
+import { Todo, List, createTodo, defaultList} from "./applogic";
 
 const firstTodo = new Todo("work", "i just need to work duh", new Date(2000,5,26), 1);
-
-const firstList = new List;
+const firstList = new List("My first list");
+createTodo("Wash my clothes", "it's self explanatory", new Date(), 2, firstList);
+createTodo("Test the default list", "it's self explanatory again", new Date(), 2);
 firstList.addTodo(firstTodo);
-console.log(firstList.array);
-firstList.removeTodo(firstTodo);
+console.log(defaultList);
 console.log(firstList.array);
